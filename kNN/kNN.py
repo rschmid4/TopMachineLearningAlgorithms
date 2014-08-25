ed029c54343286f32865b34ed3e8856e0e74d50a
Jia Shi <j5shi@live.com>
2014-08-25 15:52:09 +0800

    fixed some bugs

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -1,3 +1,4 @@
+
 """
 @author: Jia Shi, j5shi@live.com
 
@@ -6,6 +7,7 @@
 @revision history:
     - 2014-08-06 2:02:36 PM, file created.
     - 2014-08-20 4:38:16 PM, add normalization option to training set.
+    - 2014-08-25 3:14:38 PM, fixed some small bugs and unified some interfaces.
 
 @reference:
 
@@ -26,17 +28,12 @@ class kNN(object):
         @k                <int>: the number of nearest neighbors, must be a positive integer.
         @training_set <ndarray>: the training set should be a 2D array of floats, and
                                  the last column must be the label.
-        @normalize      boolean: if true, all training sets will be normalized.
+        @normalize       <bool>: if true, all training sets will be normalized.
         """
         if not isinstance(training_set, numpy.ndarray):
             raise TypeError("training set should be numpy array type.")
         else:
             self.training_set = training_set
-
-        # in case the training set is structured array, convert to normal array
-        # and we assume it contains floating numbers only.
-        if len(self.training_set.shape) == 1:
-            self.training_set = self.training_set.view('f4').reshape(self.training_set.shape[0], -1)
 
         self.k = k
         self.normalize = normalize
@@ -46,31 +43,32 @@ class kNN(object):
             raise ValueError("k out of range.")
         if name == 'normalize' and value is True:
             self.training_set = self.normalize_training_set(self.training_set)
+
         object.__setattr__(self, name, value)
 
-    def normalize_training_set(self, data_set):
-        """ normalize the given data set.
+    def normalize_training_set(self, training_set):
+        """Normalize the given data set.
 
-        @data_set <ndarray>: the data set to be normalized.
+        @training_set <ndarray>: the training set to be normalized.
         """
-        min_value_set = data_set.min(axis=0)
+        min_value_set = training_set.min(axis=0)
         min_value_set[-1] = 0               # skip the label normalization
         self.min_value_set = min_value_set
 
-        max_value_set = data_set.max(axis=0)
+        max_value_set = training_set.max(axis=0)
         max_value_set[-1] = 1               # skip the label normalization
         self.max_value_set = max_value_set
 
         ranges = max_value_set - min_value_set
         self.ranges = ranges
 
-        augment = lambda vector: numpy.tile(vector, (data_set.shape[0], 1))
-        data_set_norm = (data_set - augment(min_value_set)) / augment(ranges)
+        augment = lambda vector: numpy.tile(vector, (training_set.shape[0], 1))
+        data_set_norm = (training_set - augment(min_value_set)) / augment(ranges)
 
         return data_set_norm
 
     def __normalize_vector(self, vector):
-        """normalize a single training vector
+        """Normalize a single training vector
 
         @vector <ndarray>: a single training vector
         """
@@ -81,7 +79,7 @@ class kNN(object):
         return (vector - self.min_value_set) / self.ranges
 
     def get_euclidian_distance(self, vec0, vec1):
-        """ return the Euclidian distance of two vectors, all
+        """Return the Euclidian distance of two vectors, all
         vectors must be in same dimensions.
 
         @vec0 <ndarray>: a list of integers.
@@ -90,13 +88,20 @@ class kNN(object):
         # skip the calculate of distances of labels
         return sum([(a - b) ** 2 for a, b in zip(vec0[:-1], vec1[:-1])]) ** 0.5
 
-    def get_k_nearest_neighbors(self, vec_to_classify):
-        """ get the k nearest neighbors of the given vector to be
-        classified. k can be redefined in each call.
+    def get_label(self, vector):
+        """Return the label of a vector.
 
-        @vec_to_classify          <ndarray>: the vector be classified, must be in the
-                                             same dimension with the vector in training set.
-        @return  [(neighbor, distance),...]: return a sorted list of tuples.
+        @vector [<ndarray>, ...]: a 1xN numpy ndarray object.
+        """
+        return vector[-1]
+
+    def get_k_nearest_neighbors(self, vec_to_classify):
+        """Get the k nearest neighbors of the given vector to be
+        classified.
+
+        @vec_to_classify           <ndarray>: the vector be classified, must be in the
+                                              same dimension with the vector in training set.
+        @return  [(<ndarray>, <float>), ...]: return a sorted list of tuples of neighbors and distance.
         """
         nei, dist = range(2)
         neighbors = [(neighbor, self.get_euclidian_distance(vec_to_classify, neighbor)) for neighbor in self.training_set]
@@ -105,27 +110,28 @@ class kNN(object):
         return sorted(neighbors, key=lambda neighbor: neighbor[dist])[:self.k]
 
     def classify(self, vec_to_classify):
-        """ classify the given vector and return the label of the class
+        """Classify the given vector and return the label of the class
         into which it should be cataloged.
 
-        @vec_to_classify [float,...]: the vector be classified, must be in the
-                                      same dimension with the vector in training set.
-        @return              "label": the determined label of the class into which
-                                      the vec_to_classify to be cataloged.
+        @vec_to_classify [<float>, ...]: the vector be classified, must be in the
+                                         same dimension with the vector in training set.
+        @return                 <float>: the determined label of the class into which
+                                         the vec_to_classify to be cataloged.
         """
         if self.normalize is True:
             vec_to_classify = self.__normalize_vector(vec_to_classify)
 
         # if k == 1, then skip the majority vote and return the nearest neighbor
         if self.k == 1:
-            return self.get_k_nearest_neighbors(vec_to_classify)[0][0][-1]
+            return self.get_label(self.get_k_nearest_neighbors(vec_to_classify)[0][0])
         # do the distance weighted majority vote
         else:
             k_neighbors = self.get_k_nearest_neighbors(vec_to_classify)
 
             # weighting
             distance_max = k_neighbors[-1][1]
-            distance_delta = distance_max - k_neighbors[0][1]
+            distance_min = k_neighbors[0][1]
+            distance_delta = distance_max - distance_min
             if distance_delta == 0:
                 raise Exception("unable to break the tie of same distances!")
             else:
@@ -133,7 +139,7 @@ class kNN(object):
 
             # voting
             class_count = {}
-            for label, weight in [(neighbor[0][-1], neighbor[1]) for neighbor in k_neighbors]:
+            for label, weight in [(self.get_label(neighbor[0]), neighbor[1]) for neighbor in k_neighbors]:
                 class_count[label] = class_count.get(label, 0) + weight
 
             # sorting based on weight in descending order
@@ -148,9 +154,9 @@ if __name__ == "__main__":
                  'b': 2,
                  'c': 3}
 
-        knn = kNN(2, [[2, 2, 8, 1], [1, 7, 1, 2], [3, 3, 3, 3]])
+        knn = kNN(2, numpy.array([[2, 2, 8, 1], [1, 7, 1, 2], [3, 3, 3, 3]], dtype="f4"))
 
-        test_vec = numpy.array([6, 1, 4, 0], dtype='f4')
+        test_vec = numpy.array([2, 8, 2, 0], dtype='f4')
         # set whatever number to the label (last column), since its unknown
         print knn.get_k_nearest_neighbors(test_vec)
         print label.get(knn.classify(test_vec))
@@ -168,9 +174,9 @@ if __name__ == "__main__":
             dtype=[('f1', 'f4'), ('f2', 'f4'), ('f3', 'f4'), ('label', 'f4')],
             comments='#',
             usecols=(0, 1, 2, 3),
-            converters={3: lambda x: label[x.strip()]})
+            converters={3: lambda x: label[x.strip()]}).view("f4").reshape(-1, 4)
 
-        test_set = training_set[-const:].view('f4').reshape(const, -1)
+        test_set = training_set[-const:]
 
         knn = kNN(5, training_set)
         knn1 = kNN(5, training_set, False)
